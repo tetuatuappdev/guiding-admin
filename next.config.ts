a4c427d48e4c2f822acba0030ae66a59392ff1f5
Sylvain Finette <sylvain.sevilla@gmail.com>
2026-01-31 15:07:36 +0000

    Initial admin

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/pwa",
+        destination: "/pwa/index.html",
+      },
+      {
+        source: "/pwa/",
+        destination: "/pwa/index.html",
+      },
+      {
+        source: "/pwa/:path*",
+        destination: "/pwa/index.html",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
